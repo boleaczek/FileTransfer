4e611f1b06c3e91e02c7fe23663b5a03fe5f7651
Bartosz <bart.oleaczek@gmail.com>
2020-12-02 16:44:17 +0100

    include aesthetics and pragma once

--- src/tools/ICommandParser.h
+++ src/tools/ICommandParser.h
@@ -1,8 +1,8 @@
-#ifndef I_COMMAND_PARSER_H
-#define I_COMMAND_PARSER_H
+#pragma once
 
 #include <string>
 #include <vector>
+
 #include "PacketData.h"
 
 class ICommandParser
@@ -10,5 +10,3 @@ class ICommandParser
 public:
     virtual PacketData Parse(const std::string) = 0;
 };
-
-#endif
